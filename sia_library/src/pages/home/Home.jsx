38bfd812919bf3791468965d0d68cfe8568156bb
Lenin Patino <leninesteban98@outlook.com>
2023-10-10 12:46:04 -0500

    concluidas interfaces principales

--- sia_library/src/pages/home/Home.jsx
+++ sia_library/src/pages/home/Home.jsx
@@ -29,29 +29,35 @@ function Home() {
       </div>
       <div className=" grid grid-cols-1">
         <div className=" flex items-center justify-center">
+            <a href="/cursos">
           <Cards
             cardColor="bg-gradient-to-br from-[#3bafc4] to-[#146898]"
             cardTitle="Accede a tus cursos"
             cardDescription=""
             cardFontColor={"text-white"}
           />
+          </a>
         </div>
 
         <div className=" flex items-center justify-center">
+            <a href="/biblioteca">
           <Cards
             cardColor="bg-gradient-to-br from-[#328d49] to-[#5cc09a]"
             cardTitle="Accede a la biblioteca académica"
             cardDescription=""
             cardFontColor={"text-white"}
           />
+          </a>
         </div>
         <div className=" flex items-center justify-center">
+            <a href="/soporte">
           <Cards
             cardColor="bg-gradient-to-br from-[#ff6739] to-[#d69f39]"
             cardTitle="Soporte SIA"
             cardDescription=""
             cardFontColor={"text-white"}
           />
+          </a>
         </div>
       </div>
     </div>
--- sia_library/src/pages/support/Support.jsx
+++ sia_library/src/pages/support/Support.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Navbar from '../../components/navbar'
-Navbar
+import React from "react";
+import Navbar from "../../components/navbar";
+Navbar;
 
 function Support() {
   return (
-    <div className=' h-screen bg-[#1C274C]'><div><Navbar /></div></div>
-  )
+    <div >
+      <div>
+        <Navbar />
+      </div>
+      <div className=" mt-24 md:mt-32  mb-5 md:mb-10 mx-3 md:mx-10 flex items-center justify-center text-center text-xl">
+        <div className=" block">
+        <h3>Si presentas algún inconveniente con la plataforma, por favor comunícate a este correo:</h3>
+        <h3 className=" mt-3 text-[#146898] hover:text-"><a href="mailto:tics.plataforma@istvc.edu.ec">tics.plataforma@istvc.edu.ec</a></h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Support
+export default Support;
