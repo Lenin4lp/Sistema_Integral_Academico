e9ec95d71874bebe1411db31a204e900a2e0279f
Lenin Patino <leninesteban98@outlook.com>
2023-10-27 12:42:31 -0500

    Cree el componente de cards

--- sia_library/src/pages/courses/Courses.jsx
+++ sia_library/src/pages/courses/Courses.jsx
@@ -61,8 +61,16 @@ function Courses() {
         Mis Cursos
       </div>
       <div className=" flex justify-center items-center">
-        <div className=" grid grid-cols-3">
-          <div className=" m-3 "></div>
+        <div className=" grid grid-cols-3 mt-3">
+          <div className=" bg-gradient-to-br from-[#1C274C] to-[#146898] rounded m-3 p-3 h-[20vh] w-[40vh] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-[#31444ead] dark:hover:shadow-[#090a0e] duration-300 active:transform active:scale-90 hover:z-10">
+            <p className=" text-white text-center">Prueba</p>
+          </div>
+          <div className=" bg-gradient-to-br from-[#1C274C] to-[#146898] rounded m-3 p-3 h-[20vh] w-[40vh] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-[#31444ead] dark:hover:shadow-[#090a0e] duration-300 active:transform active:scale-90 hover:z-10">
+            <p className=" text-white text-center">Prueba</p>
+          </div>
+          <div className=" bg-gradient-to-br from-[#1C274C] to-[#146898] rounded m-3 p-3 h-[20vh] w-[40vh] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-[#31444ead] dark:hover:shadow-[#090a0e] duration-300 active:transform active:scale-90 hover:z-10">
+            <p className=" text-white text-center">Prueba</p>
+          </div>
         </div>
       </div>
     </div>
