415e85c59393aeda083bca217cd6b1c4086544f1
Lenin Patino <leninesteban98@outlook.com>
2024-01-30 11:14:40 -0500

    Agregue modales en todo el codigo

--- sia_library/src/pages/admin/degrees/ModifyDegree.jsx
+++ sia_library/src/pages/admin/degrees/ModifyDegree.jsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import { getDegree, deleteDegree } from "../../../api/academic";
 import { Link } from "react-router-dom";
 import { Toaster, toast } from "sonner";
+import Modal from "../../../components/Modal";
 
 function ModifyDegree() {
   const { register, handleSubmit } = useForm();
@@ -13,6 +14,7 @@ function ModifyDegree() {
   const [degree, setDegree] = useState();
   const navigate = useNavigate();
   const { degreeId } = useParams();
+  const [open, setOpen] = useState(false);
 
   const removeDegree = async (id) => {
     try {
@@ -20,11 +22,10 @@ function ModifyDegree() {
       if (res.status === 204) {
         toast.success("Carrera eliminada exitosamente");
         setTimeout(() => {
-          window.history.go(-1);
+          window.location.href = "/admin/carreras";
         }, 2000);
       }
     } catch (error) {
-      console.log(error);
       toast.error("Error al eliminar la carrera", {
         duration: 3000,
       });
@@ -101,6 +102,65 @@ function ModifyDegree() {
 
   return (
     <div className="my-8 lg:my-14 mx-2 md:mx-10 w-full">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className=" block m-3">
+          <div className=" flex justify-center items-center">
+            <svg
+              className="h-[100px] fill-[#ad2c2c]"
+              version="1.1"
+              id="_x32_"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 512 512"
+              xmlSpace="preserve"
+              fill=""
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <g>
+                  {" "}
+                  <path
+                    className="st0"
+                    d="M308.229,51.853C308,23.183,284.751,0.017,256,0c-28.734,0.017-52,23.183-52.228,51.853 c-63.821,9.2-109.796,33.323-109.796,49.845v16.718c0,20.784,72.538,37.625,162.024,37.625c89.486,0,162.024-16.841,162.024-37.625 v-16.718C418.024,85.176,372.049,61.053,308.229,51.853z M256,48.065c-6.245,0-12.376,0.196-18.433,0.498 c0.735-3.715,2.547-6.996,5.144-9.616c3.445-3.437,8.049-5.494,13.289-5.51c5.257,0.017,9.845,2.073,13.306,5.51 c2.595,2.62,4.408,5.902,5.135,9.616C268.384,48.261,262.245,48.065,256,48.065z"
+                  ></path>{" "}
+                  <path
+                    className="st0"
+                    d="M256,178.335c-89.486,0-162.024-16.841-162.024-37.625l18.53,316.253C112.506,478.506,167.233,512,256,512 c88.767,0,143.51-33.494,143.51-55.037l18.514-316.253C418.024,161.494,345.486,178.335,256,178.335z M158.588,421.682 l-6.661-195.134c4.465,1.02,9.249,1.878,14.269,2.743l6.752,197.878C167.763,425.436,162.988,423.567,158.588,421.682z M217.176,436.98l-3.609-202.278c4.637,0.318,9.339,0.629,14.123,0.784l3.608,202.98C226.433,438.074,221.722,437.6,217.176,436.98 z M294.824,436.98c-4.547,0.62-9.339,1.094-14.196,1.486l3.608-202.98c4.784-0.155,9.494-0.466,14.123-0.784L294.824,436.98z M353.412,421.682c-4.392,1.886-9.175,3.755-14.351,5.486l6.744-197.878c5.02-0.865,9.803-1.796,14.277-2.743L353.412,421.682z"
+                  ></path>{" "}
+                </g>{" "}
+              </g>
+            </svg>
+          </div>
+          <div className=" my-3">
+            <h1 className=" text-center text-lg font-bold">Confirmación</h1>
+          </div>
+          <div className=" my-3">
+            <h1 className=" text-center text-base font-medium">
+              ¿Estás seguro de eliminar esta carrera?
+            </h1>
+          </div>
+          <div className=" flex justify-center items-center">
+            <div className=" my-2 grid grid-cols-2">
+              <div className=" mx-4">
+                <button onClick={()=> removeDegree(degreeId)} className=" p-2 active:transform active:scale-90 border border-white bg-[#ad2c2c] rounded-lg hover:bg-[#b94d4d] text-white hover:text-white text-[12px] md:text-sm lg:text-base duration-500">
+                  Eliminar
+                </button>
+              </div>
+              <div className=" mx-4">
+                <button onClick={() => setOpen(false)} className=" p-2 active:transform active:scale-90 border border-gray-400 rounded-lg hover:bg-[#146898] text-black hover:text-white text-[12px] md:text-sm lg:text-base duration-500">
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
       <div className=" h-fit block ml-1 lg:ml-10">
         <div className=" font-bold text-xl">
           <h1 className=" text-white text-xl lg:text-3xl underline underline-offset-4 decoration-2 decoration-[#146898]">
@@ -185,18 +245,19 @@ function ModifyDegree() {
                     Guardar cambios
                   </button>
                 </div>
-                <div className=" my-20">
+                
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className=" flex justify-center items-center my-20">
                   <button
-                    onClick={() => removeDegree(degreeId)}
+                    onClick={() => setOpen(true)}
                     className=" p-2 active:transform active:scale-90 bg-white rounded-lg hover:bg-[#981426] text-[#1C274C] hover:text-white text-sm lg:text-base duration-500"
                   >
                     Eliminar carrera
                   </button>
                 </div>
-              </div>
-            </div>
-          </form>
-        </div>
         <div className=" flex justify-center items-center">
           <div className=" block">
             {errors && errors.length > 0 && (
--- sia_library/src/pages/admin/users/ModifyUser.jsx
+++ sia_library/src/pages/admin/users/ModifyUser.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { getDegrees, getModalities } from "../../../api/academic";
-import { updateStudent, updateUser, getUser } from "../../../api/user";
+import {
+  updateStudent,
+  updateUser,
+  getUser,
+  deleteUser,
+} from "../../../api/user";
 import { useNavigate } from "react-router-dom";
 import { getPeriods } from "../../../api/academic";
 import { useParams } from "react-router-dom";
@@ -9,6 +14,7 @@ import { Link } from "react-router-dom";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import { Toaster, toast } from "sonner";
+import Modal from "../../../components/Modal";
 
 function ModifyUser() {
   const { register, handleSubmit } = useForm();
@@ -21,6 +27,23 @@ function ModifyUser() {
   const [content, setContent] = useState(1);
   const [user, setUser] = useState();
   const { userId } = useParams();
+  const [open, setOpen] = useState(false);
+
+  const removeUser = async (id) => {
+    try {
+      const res = await deleteUser(id);
+      if (res.status === 204) {
+        toast.success("Estudiante eliminado exitosamente");
+        setTimeout(() => {
+          window.history.go(-1);
+        }, 2000);
+      }
+    } catch (error) {
+      toast.error("Error al eliminar el estudiante", {
+        duration: 3000,
+      });
+    }
+  }
 
   const generatePDF = async () => {
     const doc = new jsPDF();
@@ -393,6 +416,65 @@ function ModifyUser() {
 
   return (
     <div className="my-8 lg:my-14 mx-2 md:mx-10 w-full">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className=" block m-3">
+          <div className=" flex justify-center items-center">
+            <svg
+              className="h-[100px] fill-[#ad2c2c]"
+              version="1.1"
+              id="_x32_"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 512 512"
+              xmlSpace="preserve"
+              fill=""
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <g>
+                  {" "}
+                  <path
+                    className="st0"
+                    d="M308.229,51.853C308,23.183,284.751,0.017,256,0c-28.734,0.017-52,23.183-52.228,51.853 c-63.821,9.2-109.796,33.323-109.796,49.845v16.718c0,20.784,72.538,37.625,162.024,37.625c89.486,0,162.024-16.841,162.024-37.625 v-16.718C418.024,85.176,372.049,61.053,308.229,51.853z M256,48.065c-6.245,0-12.376,0.196-18.433,0.498 c0.735-3.715,2.547-6.996,5.144-9.616c3.445-3.437,8.049-5.494,13.289-5.51c5.257,0.017,9.845,2.073,13.306,5.51 c2.595,2.62,4.408,5.902,5.135,9.616C268.384,48.261,262.245,48.065,256,48.065z"
+                  ></path>{" "}
+                  <path
+                    className="st0"
+                    d="M256,178.335c-89.486,0-162.024-16.841-162.024-37.625l18.53,316.253C112.506,478.506,167.233,512,256,512 c88.767,0,143.51-33.494,143.51-55.037l18.514-316.253C418.024,161.494,345.486,178.335,256,178.335z M158.588,421.682 l-6.661-195.134c4.465,1.02,9.249,1.878,14.269,2.743l6.752,197.878C167.763,425.436,162.988,423.567,158.588,421.682z M217.176,436.98l-3.609-202.278c4.637,0.318,9.339,0.629,14.123,0.784l3.608,202.98C226.433,438.074,221.722,437.6,217.176,436.98 z M294.824,436.98c-4.547,0.62-9.339,1.094-14.196,1.486l3.608-202.98c4.784-0.155,9.494-0.466,14.123-0.784L294.824,436.98z M353.412,421.682c-4.392,1.886-9.175,3.755-14.351,5.486l6.744-197.878c5.02-0.865,9.803-1.796,14.277-2.743L353.412,421.682z"
+                  ></path>{" "}
+                </g>{" "}
+              </g>
+            </svg>
+          </div>
+          <div className=" my-3">
+            <h1 className=" text-center text-lg font-bold">Confirmación</h1>
+          </div>
+          <div className=" my-3">
+            <h1 className=" text-center text-base font-medium">
+              ¿Estás seguro de eliminar este usuario?
+            </h1>
+          </div>
+          <div className=" flex justify-center items-center">
+            <div className=" my-2 grid grid-cols-2">
+              <div className=" mx-4">
+                <button onClick={()=> removeUser(userId)} className=" p-2 active:transform active:scale-90 border border-white bg-[#ad2c2c] rounded-lg hover:bg-[#b94d4d] text-white hover:text-white text-[12px] md:text-sm lg:text-base duration-500">
+                  Eliminar
+                </button>
+              </div>
+              <div className=" mx-4">
+                <button onClick={() => setOpen(false)} className=" p-2 active:transform active:scale-90 border border-gray-400 rounded-lg hover:bg-[#146898] text-black hover:text-white text-[12px] md:text-sm lg:text-base duration-500">
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
       {content === 1 && (
         <div className=" h-fit block ml-1 lg:ml-10">
           <div className=" font-bold text-xl">
@@ -571,6 +653,11 @@ function ModifyUser() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className=" w-full flex justify-center items-center m-10">
+            <button onClick={() => setOpen(true)} className=" p-2 active:transform active:scale-90 border border-white rounded-lg hover:bg-[#bb4a4a] text-white hover:text-white text-[13px] duration-500">
+              Eliminar usuario
+            </button>
           </div>
         </div>
       )}
@@ -758,9 +845,8 @@ function ModifyUser() {
                           <select
                             className=" w-42 md:w-56 lg:w-full bg-white text-[1rem] font-normal placeholder-[#1c274cbb] text-[#1c274c] border border-gray-200 rounded py-2 px-1 mt-3"
                             {...register("modality_id")}
-                            
                           >
-                          <option value={""}>Seleccione una modalidad</option>
+                            <option value={""}>Seleccione una modalidad</option>
                             {modalities.map((modality) => (
                               <option
                                 value={modality.modality_id}
